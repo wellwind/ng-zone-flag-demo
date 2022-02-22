6647d903022c3fe9ec4b418440bc6bbd478eacb7
Mike <wellwind@mail2000.com.tw>
2022-02-22 15:58:49 +0800

    reproduce __Zone_disable_xxx flag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'ng-zone-flag-demo';
+export class AppComponent implements AfterViewInit {
+  value1 = 0;
+  value2 = 0;
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.value1 = 1;
+    }, 1000);
+
+    requestAnimationFrame(() => {
+      this.value2 = 2;
+    });
+  }
 }
